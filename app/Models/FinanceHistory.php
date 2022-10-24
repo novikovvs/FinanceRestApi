786e1f853e99b5fa987f893c9c30ee0ac29328ed
novikovvs <115675448+novikovvs@users.noreply.github.com>
2022-10-25 00:29:12 +0300

    Add ide-helper

--- app/Models/FinanceHistory.php
+++ app/Models/FinanceHistory.php
@@ -5,6 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\FinanceHistory
+ *
+ * @property int $id
+ * @property string $operation_datetime
+ * @property float $amount
+ * @property string|null $category
+ * @property int|null $MSS
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory whereMSS($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory whereOperationDatetime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FinanceHistory whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class FinanceHistory extends Model
 {
     use HasFactory;
--- app/Models/ImportCSV.php
+++ app/Models/ImportCSV.php
@@ -12,6 +12,26 @@ use Spatie\MediaLibrary\MediaCollections\FileAdder;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
+/**
+ * App\Models\ImportCSV
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $path
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection|Media[] $media
+ * @property-read int|null $media_count
+ * @method static \Illuminate\Database\Eloquent\Builder|ImportCSV newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ImportCSV newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ImportCSV query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ImportCSV whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ImportCSV whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ImportCSV whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ImportCSV wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ImportCSV whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class ImportCSV extends Model implements HasMedia
 {
     use InteractsWithMedia;
