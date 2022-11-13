c7e614987ee55643d4fd5a236ca8b5a231c5892c
novikovvs <115675448+novikovvs@users.noreply.github.com>
2022-11-13 05:29:47 +0300

    version 0.0.1

---
+++ app/FinanceAnalyzer/Jobs/AnalyticJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\FinanceAnalyzer\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AnalyticJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct() {}
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+    }
+}
